5e5f565b02e9eb47fcce1d90e51408871861f160
Elena Kolykhalova <e.n.kolykhalova@gmail.com>
2023-08-25 09:18:00 +0300

    correct output roman

--- main.go
+++ main.go
@@ -125,11 +125,17 @@ func toRoman(n int) string {
 		value  int
 		symbol string
 	}{
+		{1000, "M"},
+		{500, "D"},
+		{100, "C"},
+		{90, "XC"},
+		{50, "L"},
+		{40, "XL"},
 		{10, "X"},
 		{9, "IX"},
 		{5, "V"},
 		{4, "IV"},
-		{1, "I"},
+		{1, "I"},	
 	} {
 		for n >= r.value {
 			result.WriteString(r.symbol)
